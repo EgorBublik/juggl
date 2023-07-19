b456ea1e01a9ef3a00cb3beebdb550c4b69f6a8e
EgorBublik <egorka.bublik@mail.ru>
2023-07-19 11:03:03 +0300

    add left Menu and Team

--- client/src/components/Team/Team.js
+++ client/src/components/Team/Team.js
@@ -1,6 +1,10 @@
 import './team.css';
 import AddIcon from '../icons/add.svg'
 import ImportIcon from '../icons/import.svg'
+import User1 from '../icons/user1.svg'
+import User2 from '../icons/user2.svg'
+import User3 from '../icons/user3.svg'
+import User4 from '../icons/user4.svg'
 
 function Team() {
   return (
@@ -28,14 +32,49 @@ function Team() {
         <div className="team-table">
             <div className="team-filters">
                 <div className="team-search">
-                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M1.66659 6.33331C1.66659 3.75598 3.75592 1.66665 6.33325 1.66665C8.91058 1.66665 10.9999 3.75598 10.9999 6.33331C10.9999 7.59063 10.5027 8.73182 9.69413 9.57096C9.67134 9.58849 9.64941 9.60769 9.62853 9.62858C9.60765 9.64946 9.58844 9.67138 9.57091 9.69418C8.73177 10.5027 7.59058 11 6.33325 11C3.75592 11 1.66659 8.91064 1.66659 6.33331ZM10.0784 11.0212C9.05194 11.8423 7.74993 12.3333 6.33325 12.3333C3.01954 12.3333 0.333252 9.64702 0.333252 6.33331C0.333252 3.0196 3.01954 0.333313 6.33325 0.333313C9.64696 0.333313 12.3333 3.0196 12.3333 6.33331C12.3333 7.74998 11.8423 9.05198 11.0212 10.0784L13.4713 12.5286C13.7317 12.7889 13.7317 13.211 13.4713 13.4714C13.211 13.7317 12.7889 13.7317 12.5285 13.4714L10.0784 11.0212Z" fill="#C5B1FE"/>
-                </svg>
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1.66659 6.33331C1.66659 3.75598 3.75592 1.66665 6.33325 1.66665C8.91058 1.66665 10.9999 3.75598 10.9999 6.33331C10.9999 7.59063 10.5027 8.73182 9.69413 9.57096C9.67134 9.58849 9.64941 9.60769 9.62853 9.62858C9.60765 9.64946 9.58844 9.67138 9.57091 9.69418C8.73177 10.5027 7.59058 11 6.33325 11C3.75592 11 1.66659 8.91064 1.66659 6.33331ZM10.0784 11.0212C9.05194 11.8423 7.74993 12.3333 6.33325 12.3333C3.01954 12.3333 0.333252 9.64702 0.333252 6.33331C0.333252 3.0196 3.01954 0.333313 6.33325 0.333313C9.64696 0.333313 12.3333 3.0196 12.3333 6.33331C12.3333 7.74998 11.8423 9.05198 11.0212 10.0784L13.4713 12.5286C13.7317 12.7889 13.7317 13.211 13.4713 13.4714C13.211 13.7317 12.7889 13.7317 12.5285 13.4714L10.0784 11.0212Z" fill="#C5B1FE"/>
+                    </svg>
                     <input type="text" placeholder="Search"/>
                 </div>
 
                 <div className="team-filter-control">
-                    test
+                    <div className="team-filter-control-input">
+                        Type
+                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.528514 0.528575C0.788864 0.268226 1.21097 0.268226 1.47132 0.528575L4.99992 4.05717L8.52851 0.528575C8.78886 0.268226 9.21097 0.268226 9.47132 0.528575C9.73167 0.788925 9.73167 1.21103 9.47132 1.47138L5.47132 5.47138C5.21097 5.73173 4.78886 5.73173 4.52851 5.47138L0.528514 1.47138C0.268165 1.21103 0.268165 0.788925 0.528514 0.528575Z" fill="#C5B1FE"/>
+                        </svg>
+                    </div>
+                    <div className="team-filter-control-input">
+                        Project
+                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.528514 0.528575C0.788864 0.268226 1.21097 0.268226 1.47132 0.528575L4.99992 4.05717L8.52851 0.528575C8.78886 0.268226 9.21097 0.268226 9.47132 0.528575C9.73167 0.788925 9.73167 1.21103 9.47132 1.47138L5.47132 5.47138C5.21097 5.73173 4.78886 5.73173 4.52851 5.47138L0.528514 1.47138C0.268165 1.21103 0.268165 0.788925 0.528514 0.528575Z" fill="#C5B1FE"/>
+                        </svg>
+                    </div>
+                    <div className="team-filter-control-input">
+                        Team
+                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.528514 0.528575C0.788864 0.268226 1.21097 0.268226 1.47132 0.528575L4.99992 4.05717L8.52851 0.528575C8.78886 0.268226 9.21097 0.268226 9.47132 0.528575C9.73167 0.788925 9.73167 1.21103 9.47132 1.47138L5.47132 5.47138C5.21097 5.73173 4.78886 5.73173 4.52851 5.47138L0.528514 1.47138C0.268165 1.21103 0.268165 0.788925 0.528514 0.528575Z" fill="#C5B1FE"/>
+                        </svg>
+                    </div>
+                    <div className="team-filter-control-input">
+                        Country
+                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.528514 0.528575C0.788864 0.268226 1.21097 0.268226 1.47132 0.528575L4.99992 4.05717L8.52851 0.528575C8.78886 0.268226 9.21097 0.268226 9.47132 0.528575C9.73167 0.788925 9.73167 1.21103 9.47132 1.47138L5.47132 5.47138C5.21097 5.73173 4.78886 5.73173 4.52851 5.47138L0.528514 1.47138C0.268165 1.21103 0.268165 0.788925 0.528514 0.528575Z" fill="#C5B1FE"/>
+                        </svg>
+                    </div>
+                    <div className="team-filter-control-input">
+                        Status
+                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.528514 0.528575C0.788864 0.268226 1.21097 0.268226 1.47132 0.528575L4.99992 4.05717L8.52851 0.528575C8.78886 0.268226 9.21097 0.268226 9.47132 0.528575C9.73167 0.788925 9.73167 1.21103 9.47132 1.47138L5.47132 5.47138C5.21097 5.73173 4.78886 5.73173 4.52851 5.47138L0.528514 1.47138C0.268165 1.21103 0.268165 0.788925 0.528514 0.528575Z" fill="#C5B1FE"/>
+                        </svg>
+                    </div>
+                    <div className="team-filter-control-reset">
+                        Reset
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.47136 2.86195C8.21101 2.6016 7.7889 2.6016 7.52855 2.86195C7.26821 3.1223 7.26821 3.54441 7.52855 3.80476L11.0571 7.33335H3.33329C2.9651 7.33335 2.66663 7.63183 2.66663 8.00002C2.66663 8.36821 2.9651 8.66669 3.33329 8.66669H11.0571L7.52855 12.1953C7.26821 12.4556 7.26821 12.8777 7.52855 13.1381C7.7889 13.3984 8.21101 13.3984 8.47136 13.1381L13.1376 8.4719C13.1392 8.47029 13.1408 8.46868 13.1424 8.46705C13.26 8.34721 13.3328 8.18311 13.3333 8.00202C13.3333 8.00135 13.3333 8.00069 13.3333 8.00002C13.3333 7.99935 13.3333 7.99869 13.3333 7.99802C13.333 7.90836 13.3151 7.82286 13.2827 7.74483C13.2509 7.66799 13.2041 7.59588 13.1424 7.53299C13.1408 7.53139 13.1392 7.52979 13.1376 7.52821M8.47136 2.86195L13.1376 7.52821L8.47136 2.86195Z" fill="#C5B1FE"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
             
@@ -59,22 +98,175 @@ function Team() {
             
             <div className="team-table-list">
                 <table>
-                    <tr>
-                        <td>
-                            12
+
+                <tr>
+                        <td className='team-table-list-header'>
+                            <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.13807 0.528575C0.877722 0.268226 0.455612 0.268226 0.195262 0.528575C-0.0650874 0.788925 -0.0650874 1.21103 0.195262 1.47138L3.5286 4.80472C3.78894 5.06507 4.21106 5.06507 4.4714 4.80472L7.80474 1.47138C8.06509 1.21103 8.06509 0.788925 7.80474 0.528575C7.54439 0.268226 7.12228 0.268226 6.86193 0.528575L4 3.3905L1.13807 0.528575ZM1.13807 5.19524C0.877722 4.93489 0.455612 4.93489 0.195262 5.19524C-0.0650874 5.45559 -0.0650874 5.8777 0.195262 6.13805L3.5286 9.47138C3.78894 9.73173 4.21106 9.73173 4.4714 9.47138L7.80474 6.13805C8.06509 5.8777 8.06509 5.45559 7.80474 5.19524C7.54439 4.93489 7.12228 4.93489 6.86193 5.19524L4 8.05717L1.13807 5.19524Z" fill="#B3B5B7"/>
+                            </svg>
+                             Member
                         </td>
-                        <td>
-                            34
+                        
+                        <td className='team-table-list-header col-table'>
+                            <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.13807 0.528575C0.877722 0.268226 0.455612 0.268226 0.195262 0.528575C-0.0650874 0.788925 -0.0650874 1.21103 0.195262 1.47138L3.5286 4.80472C3.78894 5.06507 4.21106 5.06507 4.4714 4.80472L7.80474 1.47138C8.06509 1.21103 8.06509 0.788925 7.80474 0.528575C7.54439 0.268226 7.12228 0.268226 6.86193 0.528575L4 3.3905L1.13807 0.528575ZM1.13807 5.19524C0.877722 4.93489 0.455612 4.93489 0.195262 5.19524C-0.0650874 5.45559 -0.0650874 5.8777 0.195262 6.13805L3.5286 9.47138C3.78894 9.73173 4.21106 9.73173 4.4714 9.47138L7.80474 6.13805C8.06509 5.8777 8.06509 5.45559 7.80474 5.19524C7.54439 4.93489 7.12228 4.93489 6.86193 5.19524L4 8.05717L1.13807 5.19524Z" fill="#B3B5B7"/>
+                            </svg>
+                            Type
+                        </td>
+                        
+                        <td className='team-table-list-header col-table'>
+                            <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.13807 0.528575C0.877722 0.268226 0.455612 0.268226 0.195262 0.528575C-0.0650874 0.788925 -0.0650874 1.21103 0.195262 1.47138L3.5286 4.80472C3.78894 5.06507 4.21106 5.06507 4.4714 4.80472L7.80474 1.47138C8.06509 1.21103 8.06509 0.788925 7.80474 0.528575C7.54439 0.268226 7.12228 0.268226 6.86193 0.528575L4 3.3905L1.13807 0.528575ZM1.13807 5.19524C0.877722 4.93489 0.455612 4.93489 0.195262 5.19524C-0.0650874 5.45559 -0.0650874 5.8777 0.195262 6.13805L3.5286 9.47138C3.78894 9.73173 4.21106 9.73173 4.4714 9.47138L7.80474 6.13805C8.06509 5.8777 8.06509 5.45559 7.80474 5.19524C7.54439 4.93489 7.12228 4.93489 6.86193 5.19524L4 8.05717L1.13807 5.19524Z" fill="#B3B5B7"/>
+                            </svg>
+                            Project
+                        </td>
+                        
+                        <td className='team-table-list-header col-table'>
+                            <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.13807 0.528575C0.877722 0.268226 0.455612 0.268226 0.195262 0.528575C-0.0650874 0.788925 -0.0650874 1.21103 0.195262 1.47138L3.5286 4.80472C3.78894 5.06507 4.21106 5.06507 4.4714 4.80472L7.80474 1.47138C8.06509 1.21103 8.06509 0.788925 7.80474 0.528575C7.54439 0.268226 7.12228 0.268226 6.86193 0.528575L4 3.3905L1.13807 0.528575ZM1.13807 5.19524C0.877722 4.93489 0.455612 4.93489 0.195262 5.19524C-0.0650874 5.45559 -0.0650874 5.8777 0.195262 6.13805L3.5286 9.47138C3.78894 9.73173 4.21106 9.73173 4.4714 9.47138L7.80474 6.13805C8.06509 5.8777 8.06509 5.45559 7.80474 5.19524C7.54439 4.93489 7.12228 4.93489 6.86193 5.19524L4 8.05717L1.13807 5.19524Z" fill="#B3B5B7"/>
+                            </svg>
+                            Team
+                        </td>
+                        
+                        <td className='team-table-list-header col-table'>
+                            <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.13807 0.528575C0.877722 0.268226 0.455612 0.268226 0.195262 0.528575C-0.0650874 0.788925 -0.0650874 1.21103 0.195262 1.47138L3.5286 4.80472C3.78894 5.06507 4.21106 5.06507 4.4714 4.80472L7.80474 1.47138C8.06509 1.21103 8.06509 0.788925 7.80474 0.528575C7.54439 0.268226 7.12228 0.268226 6.86193 0.528575L4 3.3905L1.13807 0.528575ZM1.13807 5.19524C0.877722 4.93489 0.455612 4.93489 0.195262 5.19524C-0.0650874 5.45559 -0.0650874 5.8777 0.195262 6.13805L3.5286 9.47138C3.78894 9.73173 4.21106 9.73173 4.4714 9.47138L7.80474 6.13805C8.06509 5.8777 8.06509 5.45559 7.80474 5.19524C7.54439 4.93489 7.12228 4.93489 6.86193 5.19524L4 8.05717L1.13807 5.19524Z" fill="#B3B5B7"/>
+                            </svg>
+                            Country
+                        </td>
+                        
+                        <td className='team-table-list-header team-table-list-contract col-table'>
+                            <svg width="16" height="16" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.4714 0.528575C4.21106 0.268226 3.78894 0.268226 3.5286 0.528575L0.195262 3.86191C-0.0650874 4.12226 -0.0650874 4.54437 0.195262 4.80472C0.455612 5.06507 0.877722 5.06507 1.13807 4.80472L4 1.94279L6.86193 4.80472C7.12228 5.06507 7.54439 5.06507 7.80474 4.80472C8.06509 4.54437 8.06509 4.12226 7.80474 3.86191L4.4714 0.528575ZM7.80474 8.52858L4.4714 5.19524C4.21106 4.93489 3.78894 4.93489 3.5286 5.19524L0.195262 8.52858C-0.0650874 8.78893 -0.0650874 9.21103 0.195262 9.47138C0.455612 9.73173 0.877722 9.73173 1.13807 9.47138L4 6.60946L6.86193 9.47138C7.12228 9.73173 7.54439 9.73173 7.80474 9.47138C8.06509 9.21103 8.06509 8.78893 7.80474 8.52858Z" fill="#C5B1FE"/>
+                            </svg>
+                            Contract
                         </td>
                     </tr>
+
                     <tr>
                         <td>
-                            12
+                            <img className='user-image' src={User1} alt='user'/>
+                            <div className="team-table-list-user ">
+                                <div className='team-table-list-name'>Giacomina Otaegui</div>
+                                <div className='team-table-list-post'>Cloud Security specialist </div>
+                            </div>
+                            
                         </td>
-                        <td>
-                            34
+                        
+                        <td className='team-table-list-type col-table'>
+                            Employee
+                        </td>
+                        
+                        <td className='team-table-list-project col-table'>
+                            ASF
+                        </td>
+                        
+                        <td className='team-table-list-team col-table'>
+                            Marketing
+                        </td>
+                        
+                        <td className='team-table-list-country col-table'>
+                            Spain
+                        </td>
+                        
+                        <td className='team-table-list-contract-disabled col-table'>
+                            <span>TERMINATED</span> 
                         </td>
                     </tr>
+                    
+                    <tr>
+                        <td>
+                            <img className='user-image' src={User2} alt='user'/>
+                            <div className="team-table-list-user">
+                                <div className='team-table-list-name'>Tilda Ulrich</div>
+                                <div className='team-table-list-post'>SEO</div>
+                            </div>
+                            
+                        </td>
+                        
+                        <td className='team-table-list-type col-table'>
+                            Employee
+                        </td>
+                        
+                        <td className='team-table-list-project col-table'>
+                            ASF
+                        </td>
+                        
+                        <td className='team-table-list-team col-table'>
+                            Development...
+                        </td>
+                        
+                        <td className='team-table-list-country col-table'>
+                            Spain
+                        </td>
+                        
+                        <td className='team-table-list-contract-active col-table'>
+                            <span>ACTIVE</span> 
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <img className='user-image' src={User3} alt='user'/>
+                            <div className="team-table-list-user">
+                                <div className='team-table-list-name'>Sam Sauno</div>
+                                <div className='team-table-list-post'>Fullstack</div>
+                            </div>
+                            
+                        </td>
+                        
+                        <td className='team-table-list-type col-table'>
+                            Contractor
+                        </td>
+                        
+                        <td className='team-table-list-project col-table'>
+                            DDT
+                        </td>
+                        
+                        <td className='team-table-list-team col-table'>
+                            Development...
+                        </td>
+                        
+                        <td className='team-table-list-country col-table'>
+                            Germany
+                        </td>
+                        
+                        <td className='team-table-list-contract-active col-table'>
+                            <span>ACTIVE</span> 
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <img className='user-image' src={User4} alt='user'/>
+                            <div className="team-table-list-user">
+                                <div className='team-table-list-name'>Peter Stadnik</div>
+                                <div className='team-table-list-post'>PR-specialist</div>
+                            </div>
+                            
+                        </td>
+                        
+                        <td className='team-table-list-type col-table'>
+                            Employee
+                        </td>
+                        
+                        <td className='team-table-list-project col-table'>
+                            My lovely Pro...
+                        </td>
+                        
+                        <td className='team-table-list-team col-table'>
+                            Development...
+                        </td>
+                        
+                        <td className='team-table-list-country col-table'>
+                            US
+                        </td>
+                        
+                        <td className='team-table-list-contract-active col-table'>
+                            <span>ACTIVE</span> 
+                        </td>
+                    </tr>
+
                 </table>
             </div>
         </div>
